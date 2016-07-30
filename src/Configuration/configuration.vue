4a4c625f13c4c7aed8953112c9a409b9d83b546c
Max <maxicespedes7@hotmail.com>
2016-07-30 00:01:39 -0300

    added empty modal

---
+++ src/Configuration/configuration.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <modal :show.sync="show">
+       <h3 slot="header">Configuration</h3>
+
+  </modal>
+</template>
+
+<script>
+import modal from '../components/modal/modal'
+
+
+export default {
+  components:{
+    modal
+  },
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+      twoWay: true
+    }
+  },
+  data () {
+    return {
+  
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/components/chead.vue
+++ src/components/chead.vue
@@ -1,6 +1,7 @@
 <template>
+  <configuration :show.sync="showModal"></configuration>
   <div class="chead">
-    <i class="config fa fa-cog fa-lg" aria-hidden="true"></i>
+    <i @click="showConfig()" class="config fa fa-cog fa-lg" aria-hidden="true"></i>
     <h1>{{ msg }}</h1>
     <div class="">
 
@@ -9,14 +10,27 @@
 </template>
 
 <script>
+import configuration from '../configuration/configuration'
+
 export default {
+  components:{
+    configuration
+  },
+  methods:{
+    showConfig: function(){
+      console.log('logging');
+      console.log(this.showModal);
+      this.showModal=true;
+    }
+  },
   data () {
     return {
       // note: changing this line won't causes changes
       // with hot-reload because the reloaded component
       // preserves its current state and we are modifying
       // its initial state.
-      msg: 'LifeBox'
+      msg: 'LifeBox',
+      showModal:false
     }
   }
 }
